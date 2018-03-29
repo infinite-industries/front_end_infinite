8fe4afee76dff82dfcc7941182f5eb7c87b9d27b
dima <shifting.planes@gmail.com>
2018-03-29 19:53:59 -0400

    added router

--- src/main.js
+++ src/main.js
@@ -6,11 +6,29 @@ import Vuetify from 'vuetify';
 
 import App from './App.vue';
 
+import Home from './pages/Home.vue';
+import SubmitEvent from './pages/SubmitEvent.vue'
+
 Vue.use(VueRouter);
 Vue.use(Vuetify);
 
+const router = new VueRouter({
+    routes: [
+      {
+        path: '/',
+        name: 'home',
+        component: Home
+      },
+      {
+        path:'/submit-event',
+        name: 'submit_event',
+        component: SubmitEvent
+      }
+    ]
+})
+
 const app = new Vue({
-    // router,
+    router: router,
     render: createEle => createEle(App),
     beforeCreate: function(){
       console.log("started app");
