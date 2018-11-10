683ef45e71722bd9bc3c6a2676deaf383ec63af4
dima <shifting.planes@gmail.com>
2018-11-10 18:56:14 -0500

    hacked out a really ugly solution for ics files. need to really investigate if there is a serious issue with momentjs

--- routes/calendar.js
+++ routes/calendar.js
@@ -25,12 +25,17 @@ BEGIN:VEVENT
 UID:${uuidv4()}
 SUMMARY:${req.query.title}
 DTSTAMP:${moment().utc().format('YYYYMMDDTHHmmss')}Z
-DTSTART:${moment(req.query.time_start).utc().format('YYYYMMDDTHHmmss')}Z
-DTEND:${moment(req.query.time_end).utc().format('YYYYMMDDTHHmmss')}Z
+DTSTART:${moment(req.query.time_start).format('YYYYMMDDTHHmmss')}
+DTEND:${moment(req.query.time_end).format('YYYYMMDDTHHmmss')}
 DESCRIPTION:${req.query.description}
 LOCATION:${req.query.location}
 END:VEVENT
 END:VCALENDAR`
+
+// The correct way of working with time
+// Current approach is a hack :(
+// DTSTART:${moment(req.query.time_start).utc().format('YYYYMMDDTHHmmss')}Z
+// DTEND:${moment(req.query.time_end).utc().format('YYYYMMDDTHHmmss')}Z
 
   console.log(req.query.time_start +"\n ------- \n")
   console.log("req:"+req.query.time_start+" utc:"+moment(req.query.time_start).utc().format('YYYYMMDDTHHmmss'))
